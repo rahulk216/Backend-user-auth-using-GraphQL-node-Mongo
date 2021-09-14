e59f249b7c052a80c1870482152bfab2bca8a9a5
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2021-09-14 19:33:41 +0530

    login

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,46 +1,96 @@
-const User = require('../../models/User');
 const md5 = require('md5');
 const jwt = require('jsonwebtoken');
-const { SECRET_KEY } = require('../../config.js');
-const {UserInputError} = require('apollo-server')
+const { UserInputError } = require('apollo-server');
+const {
+  validateRegisterInput,
+  validateLoginInput
+} = require('../../utils/validators');
+const { SECRET_KEY } = require('../../config');
+const User = require('../../models/User');
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  );
+}
 
 module.exports = {
-	Mutation: {
-		async register(
-			_,
-			{ registerInput: { username, password, email, confirmPassword } }
-		) {
+  Mutation: {
+	  async login (_,{username,password}){
+		const { valid, errors } = validateLoginInput(
+        username,
+        password,
+		);
+		if (!valid) {
+			throw new UserInputError('Errors', { errors });
+		}
+		const user = await User.findOne({ username });
+		if(!user)
+		{
+			errors.general='user not found';
+			throw new UserInputError('user not found', { errors });
+		}
+		if(!(user.password===md5(password)))
+		{
+			throw new UserInputError('user not found', { errors });
+		}
+		const token =generateToken(user)
+		 return {
+        ...user._doc,
+        id: user._id,
+        token
+      };
+	  },
+    async register(
+      _,
+      {
+        registerInput: { username, email, password, confirmPassword }
+      }
+    ) {
+      // Validate user data
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError('Errors', { errors });
+      }
+      // TODO: Make sure user doesnt already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'This username is taken'
+          }
+        });
+      }
+      // hash password and create an auth token
+      password = md5(password);
 
-			//validate user from db
-			const user = await User.findOne({ username })
-			console.log(user)
-			if(user)
-			{ 
-				throw new UserInputError('username is taken')
-			}
-			password = await md5(password);
-			const newUser = new User({
-				email,
-				username,
-				password,
-				createdAt: new Date().toISOString(),
-			});
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString()
+      });
 
-			const res = await newUser.save();
-			const token = jwt.sign(
-				{
-					id: res.id,
-					email: res.email,
-					username: res.username,
-				},
-				SECRET_KEY,
-				{ expiresIn: '1hr' }
-			);
-			return {
-				...res._doc,
-				id: res._id,
-				token,
-			};
-		},
-	},
+      const res = await newUser.save();
+
+      const token = generateToken(res);
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token
+      };
+    }
+  }
 };
