78fe8a12737b5201fa30a1217a39b64c3df9a3ae
Rahul Kakkadan <rahulkakkadan216@gmail.com>
2021-09-16 10:32:15 +0530

    comments.likes

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,9 +1,9 @@
 const Post = require('../../models/Post');
-const checkAuth = require('../../utils/checkAuth')
-const { AuthenticationError } = require('apollo-server')
+const checkAuth = require('../../utils/checkAuth');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 module.exports = {
-    Query: {
+	Query: {
 		async getPosts() {
 			try {
 				const posts = await Post.find().sort({ createdAt: -1 });
@@ -12,49 +12,107 @@ module.exports = {
 				throw new Error(err);
 			}
 		},
-		async getPost(_,{ postId }){
-			console.log(postId)
-			try{
-				const post = await Post.findById(postId)
-				console.log(post)
-				if(post){
-					return post
+		async getPost(_, { postId }) {
+			console.log(postId);
+			try {
+				const post = await Post.findById(postId);
+				console.log(post);
+				if (post) {
+					return post;
+				} else {
+					throw new Error('Post not found');
+				}
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+	},
+	Mutation: {
+		async createPost(_, { body }, context) {
+			const user = checkAuth(context);
+			const newPost = new Post({
+				body,
+				user: user.id,
+				username: user.username,
+				createdAt: new Date().toISOString(),
+			});
+			const post = await newPost.save();
+			return post;
+		},
+		async deletePost(_, { postId }, context) {
+			const user = checkAuth(context);
+			try {
+				const post = await Post.findById(postId);
+				if (user.username == post.username) {
+					await post.delete();
+					return 'Post deleted succesfully';
+				} else {
+					throw new Error('user not defined');
+				}
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
+		async createComment(_, { postId, body }, context) {
+			const { username } = checkAuth(context);
+			if (body.trim() === '') {
+				throw new UserInputError('Empty comment', {
+					errors: {
+						body: 'Comment body must not empty',
+					},
+				});
+			}
+			const post = await Post.findById(postId);
+			if (post) {
+				post.comments.unshift({
+					body,
+					username,
+					createdAt: new Date().toISOString(),
+				});
+				await post.save();
+				return post;
+			} else throw new UserInputError('Post not found');
+		},
+		async deleteComment(_, { postId, commentId }, context) {
+			const { username } = checkAuth(context);
+			const post = await Post.findById(postId);
+			if (post) {
+				const commentIndex = post.comments.findIndex(
+					(c) => c.id === commentId
+				);
+				if (post.comments[commentIndex].username === username) {
+					post.comments.splice(commentIndex, 1);
+					await post.save();
+					return post;
+				} else {
+					throw new AuthenticationError('Action not allowed');
+				}
+			}else{
+				throw new UserInputError('Post not found');
+			}
+		},
+		async likePost(_,{ postId }, context){
+			const { username } = checkAuth(context);
+			const post = await Post.findById(postId);
+			if(post){
+				if(post.likes.find(like=>like.username)){
+					//post already liked
+					post.likes = post.likes.filter(like=>like.username!==username);
+					await post.save();
 				}
 				else{
-					throw new Error("Post not found")
+					post.likes.push({
+						username,
+						createdAt: new Date.toISOString()
+					})
+					await post.save();
+					return post;
 				}
-			}catch(err){
-				throw new Error(err)
+			}
+			else
+			{
+				throw new UserInputError('post not found')
 			}
 		}
 	},
-	Mutation: {
-		async createPost(_,{ body }, context){
-			const user = checkAuth(context)
-			const newPost = new Post({ 
-				body, 
-				user: user.id,
-				username: user.username,
-				createdAt: new Date().toISOString()
-			})
-			const post = await newPost.save();
-			return post;
-    	},
-		async deletePost(_,{ postId }, context){
-			const user = checkAuth(context)
-			try{
-				const post = await Post.findById(postId);
-				if(user.username==post.username){
-					await post.delete();
-					return "Post deleted succesfully"
-				}
-				else{
-					throw new Error("user not defined")
-				}
-			}
-			catch(err){
-				throw new Error(err)
-			}
-    	}
-	}
-}
+};
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,28 +6,44 @@ module.exports = gql`
 		body: String!
 		createdAt: String!
 		username: String!
+		comments: [Comment]!
+		likes: [Like]!
 	}
-    input RegisterInput{
-        username: String!
-        password:String!
-        confirmPassword: String!
-        email:String!
-    }
-    type User{
-        id: ID!
-        email: String!
-        token: String!
-        username: String!
-        createdAt: String!
-    }
+	type Comment {
+		id: ID!
+		createdAt: String!
+		username: String!
+		body: String!
+	}
+	type Like {
+		id: ID!
+		createdAt: String!
+		username: String!
+	}
+	input RegisterInput {
+		username: String!
+		password: String!
+		confirmPassword: String!
+		email: String!
+	}
+	type User {
+		id: ID!
+		email: String!
+		token: String!
+		username: String!
+		createdAt: String!
+	}
 	type Query {
 		getPosts: [Post]
-        getPost(postId:ID!): Post
+		getPost(postId: ID!): Post
 	}
-    type Mutation {
-        register( registerInput: RegisterInput ): User!
-        login( username:String!,password:String!): User!
-        createPost( body: String!): Post!
-        deletePost( postId: ID! ): String!
-    }
+	type Mutation {
+		register(registerInput: RegisterInput): User!
+		login(username: String!, password: String!): User!
+		createPost(body: String!): Post!
+		deletePost(postId: ID!): String!
+		createComment(postId: String!, body: String!): Post!
+		deleteComment(postId: ID!, commentId: ID!): Post!
+		likePost(postId: ID!): Post!
+	}
 `;
